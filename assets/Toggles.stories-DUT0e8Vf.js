2d73e3e4229bdfed39b1336909f3bcb580ad1730
nickofthyme <nickofthyme@users.noreply.github.com>
2025-08-21 23:04:10 +0000

    deploy: 9497bfe47ddcbe87218ec8a1043a9ccb26fdbd8a

--- assets/Toggles.stories-DUT0e8Vf.js
+++ assets/Toggles.stories-DUT0e8Vf.js
@@ -1,15 +1,4 @@
-import{j as e,x as N,S as t}from"./iframe-BuezHbvL.js";const T=({children:P,onClick:Y})=>e.jsx(W,{type:"button",className:"my-button",onClick:Y,children:P}),W=N.button`
-  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-weight: 700;
-  border: 0;
-  border-radius: 3em;
-  cursor: pointer;
-  display: inline-block;
-  line-height: 1;
-  font-size: 14px;
-  padding: 11px 20px;
-  margin: 1rem;
-`;T.__docgenInfo={description:"Primary UI component for user interaction",methods:[],displayName:"Toggles",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""},onClick:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const k={title:"Toggles",component:T,parameters:{}},o={render:()=>e.jsx("div",{children:e.jsx("span",{children:"By default the toggle values defined in the parameters will be used. Allowing modifications from the menu bar."})})},n={render:()=>e.jsxs("div",{children:[e.jsx("span",{children:"You can replace or change global toggles at the story level using the parameters option."}),e.jsx(t,{children:`Story.parameters = {
+import{j as e,S as t}from"./iframe-BJGUt_Wa.js";const P={title:"Toggles",parameters:{}},o={render:()=>e.jsx("div",{children:e.jsx("span",{children:"By default the toggle values defined in the parameters will be used. Allowing modifications from the menu bar."})})},n={render:()=>e.jsxs("div",{children:[e.jsx("span",{children:"You can replace or change global toggles at the story level using the parameters option."}),e.jsx(t,{children:`Story.parameters = {
   toggles: {
     options: [
       {
@@ -52,7 +41,7 @@ import{j as e,x as N,S as t}from"./iframe-BuezHbvL.js";const T=({children:P,onCl
       },
     ]
   }
-}`})]}),parameters:{toggles:{options:[{id:"info-option",title:"With info",description:"This toggle has info/description",defaultValue:!1},{id:"no-info-option",title:"Without info",defaultValue:!1}]}}},r={render:()=>e.jsxs("div",{children:[e.jsxs("span",{children:["You can set the toggle disabled property to a boolean or an object. The object will keys are other toggle ids and the value that will disable the trigger.",e.jsx("br",{}),e.jsx("br",{}),"For example, the following setup would disable option-2 when option-1 is set to true. Option-3 is always disabled."]}),e.jsx(t,{children:`Story.parameters = {
+}`})]}),parameters:{toggles:{options:[{id:"info-option",title:"With info",description:"This toggle has info/description",defaultValue:!1},{id:"no-info-option",title:"Without info",defaultValue:!1}]}}},l={render:()=>e.jsxs("div",{children:[e.jsxs("span",{children:["You can set the toggle disabled property to a boolean or an object. The object will keys are other toggle ids and the value that will disable the trigger.",e.jsx("br",{}),e.jsx("br",{}),"For example, the following setup would disable option-2 when option-1 is set to true. Option-3 is always disabled."]}),e.jsx(t,{children:`Story.parameters = {
   toggles: {
     options: [
       {
@@ -76,7 +65,7 @@ import{j as e,x as N,S as t}from"./iframe-BuezHbvL.js";const T=({children:P,onCl
       },
     ]
   }
-}`})]}),parameters:{toggles:{options:[{id:"option-1",title:"Option 1",defaultValue:!1},{id:"option-2",title:"Option 2",defaultValue:!0,disabled:{"option-1":!0}},{id:"option-3",title:"Option 3",defaultValue:!0,disabled:!0}]}}};var l,d,p;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+}`})]}),parameters:{toggles:{options:[{id:"option-1",title:"Option 1",defaultValue:!1},{id:"option-2",title:"Option 2",defaultValue:!0,disabled:{"option-1":!0}},{id:"option-3",title:"Option 3",defaultValue:!0,disabled:!0}]}}};var r,d,p;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
   render: () => <div>
       <span>
         By default the toggle values defined in the parameters will be used. Allowing modifications from the menu bar.
@@ -139,7 +128,7 @@ import{j as e,x as N,S as t}from"./iframe-BuezHbvL.js";const T=({children:P,onCl
       clearable: false
     }
   }
-}`,...(m=(h=i.parameters)==null?void 0:h.docs)==null?void 0:m.source}}};var v,b,y;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+}`,...(m=(h=i.parameters)==null?void 0:h.docs)==null?void 0:m.source}}};var v,b,w;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
   render: () => <div>
       <span>
         You can override toggle values using the override option with the id and new value. This will override the default toggle values.
@@ -166,7 +155,7 @@ import{j as e,x as N,S as t}from"./iframe-BuezHbvL.js";const T=({children:P,onCl
       }
     }
   }
-}`,...(y=(b=a.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var w,x,S;s.parameters={...s.parameters,docs:{...(w=s.parameters)==null?void 0:w.docs,source:{originalSource:`{
+}`,...(w=(b=a.parameters)==null?void 0:b.docs)==null?void 0:w.source}}};var y,S,V;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
   render: () => <div>
       <span>
         You can set the description on the toggle to show an info icon that displays on hover.
@@ -206,7 +195,7 @@ import{j as e,x as N,S as t}from"./iframe-BuezHbvL.js";const T=({children:P,onCl
       }]
     }
   }
-}`,...(S=(x=s.parameters)==null?void 0:x.docs)==null?void 0:S.source}}};var V,j,O;r.parameters={...r.parameters,docs:{...(V=r.parameters)==null?void 0:V.docs,source:{originalSource:`{
+}`,...(V=(S=s.parameters)==null?void 0:S.docs)==null?void 0:V.source}}};var j,x,O;l.parameters={...l.parameters,docs:{...(j=l.parameters)==null?void 0:j.docs,source:{originalSource:`{
   render: () => <div>
       <span>
         You can set the toggle disabled property to a boolean or an object. The object will keys are other toggle ids and the value that will disable the trigger.
@@ -264,4 +253,4 @@ import{j as e,x as N,S as t}from"./iframe-BuezHbvL.js";const T=({children:P,onCl
       }]
     }
   }
-}`,...(O=(j=r.parameters)==null?void 0:j.docs)==null?void 0:O.source}}};const I=["Defaults","ReplacingToggles","UnclearableToggles","ToggleOverrides","ToggleInfoDescription","DisableToggles"];export{o as Defaults,r as DisableToggles,n as ReplacingToggles,s as ToggleInfoDescription,a as ToggleOverrides,i as UnclearableToggles,I as __namedExportsOrder,k as default};
+}`,...(O=(x=l.parameters)==null?void 0:x.docs)==null?void 0:O.source}}};const Y=["Defaults","ReplacingToggles","UnclearableToggles","ToggleOverrides","ToggleInfoDescription","DisableToggles"];export{o as Defaults,l as DisableToggles,n as ReplacingToggles,s as ToggleInfoDescription,a as ToggleOverrides,i as UnclearableToggles,Y as __namedExportsOrder,P as default};
--- assets/react-18-xo0m7jCr.js
+++ assets/react-18-xo0m7jCr.js
@@ -1,4 +1,4 @@
-import{r as Sr,g as La}from"./iframe-BuezHbvL.js";var No={exports:{}},ve={},Po={exports:{}},zo={};/**
+import{r as Sr,g as La}from"./iframe-BJGUt_Wa.js";var No={exports:{}},ve={},Po={exports:{}},zo={};/**
  * @license React
  * scheduler.production.min.js
  *
